32a3f52cfbe090816ba2a440d34dfcc50771c929
dolya-julia <juliadolya06@gmail.com>
2024-02-05 15:36:44 +0300

    lab1.task1.function2.multiply_digits_of_number_not_div_5

--- lab1/task1.py
+++ lab1/task1.py
@@ -11,6 +11,14 @@ def simple_del(a):
     if a != 1:
         l.append(a)
     return max(l)
-
+def multiply_numbers(a):
+    pr = 1
+    while a > 0:
+        k = a % 10
+        a = a // 10
+        if (k % 5 != 0):
+            pr *= k
+    return pr
 num = int(input())
 print(simple_del(num))
+print(multiply_numbers(num))
