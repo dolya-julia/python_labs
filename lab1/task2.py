a747d28f70e1f3d620137f9d082269945bb2016a
dolya-julia <juliadolya06@gmail.com>
2024-02-05 22:10:58 +0300

    lab1.task3.function2.count_even_words

--- lab1/task2.py
+++ lab1/task2.py
@@ -4,5 +4,13 @@ def mix_words(s):
     random.shuffle(l)
     s = " ".join(l)
     return s
+def count_even_words(s):
+    k = 0
+    lst = s.split()
+    for l in lst:
+        if len(l) % 2 == 0:
+            k += 1
+    return k
 str = input()
 print(mix_words(str))
+print(count_even_words(str))
