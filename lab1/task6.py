cd9e362d17e5199bdf8b076fe70e1fb0c8955bef
dolya-julia <juliadolya06@gmail.com>
2024-02-07 23:08:03 +0300

    lab1.task7.function2.latin_lower_char

--- lab1/task6.py
+++ lab1/task6.py
@@ -5,6 +5,13 @@ def count_char(s):
         if (s[i] >= 'а' and s[i] <= 'я') or s[i] == 'ё':
             k += 1
     return k
-
+def lower_char(s):
+    st = ''
+    for i in range(len(s)):
+        st += " "
+        if ord(s[i]) >= 97 and ord(s[i]) <= 122:
+            st += s[i]
+    return st
 s = input()
 print(count_char(s))
+print(lower_char(s))
