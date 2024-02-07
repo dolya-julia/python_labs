44a1fc10d96fcea78f20518ae8788ff4578a5586
dolya-julia <juliadolya06@gmail.com>
2024-02-07 20:07:54 +0300

    lab1.task4.function3.russian_flag

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,4 +1,5 @@
 import random
+import array
 def mix_words(s):
     l = s.split()
     random.shuffle(l)
@@ -11,6 +12,17 @@ def count_even_words(s):
         if len(l) % 2 == 0:
             k += 1
     return k
+def russian_flag(mas):
+    mas.sort()
+    s = " ".join(mas)
+    s_w = s.find('белый')
+    s_b = s.find('синий')
+    s_r = s.find('красный')
+    str = s[s_w:s_r] + s[s_b:] + " " + s[s_r:s_b]
+    mas = str.split()
+    return mas
 str = input()
+mas = [i for i in input().split()]
 print(mix_words(str))
 print(count_even_words(str))
+print(russian_flag(mas))
