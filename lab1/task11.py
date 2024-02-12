49bd2487bdc5eb2c2756768d63f6331125793a58
dolya-julia <juliadolya06@gmail.com>
2024-02-12 19:09:52 +0300

    lab1.task12.function2.square_deviation

--- lab1/task11.py
+++ lab1/task11.py
@@ -1,3 +1,4 @@
+import math
 def com_symbol(s):
     k = 0
     for c in s:
@@ -6,6 +7,16 @@ def com_symbol(s):
             symb = c
     st = ' '.join(l)
     return k - st.count(symb)
+def sq_deviation(s):
+    k = 0
+    for c in s:
+        if s.count(c) >= k:
+            k = s.count(c)
+            symb = c
+    st = ' '.join(l)
+    k_t = st.count(symb)
+    sr = (k + k_t) / 2
+    return math.sqrt(((k - sr) ** 2 + (k_t - sr) ** 2) / 2)
 n = int(input())
 l = [str(input()) for i in range(n)]
 l.sort(key = com_symbol)
