88c9a4fb80af22ab8e598eb28ff927156b32664b
dolya-julia <juliadolya06@gmail.com>
2024-02-13 23:06:47 +0300

    lab1.task16.function2.local_min

--- lab1/task15.py
+++ lab1/task15.py
@@ -3,7 +3,16 @@ def global_max(l, d):
         return "yes"
     else:
         return "no"
+def local_min(l, d):
+    if d == 0 or d == len(l) - 1:
+        return "no"
+    else:
+        if l[d] < l[d-1] and l[d] < l[d+1]:
+            return "yes"
+        else:
+            return "no"
+
 n = int(input())
 l = [int(input()) for i in range(n)]
 d = int(input())
-print(global_max(l, d))
+print(local_min(l, d))
