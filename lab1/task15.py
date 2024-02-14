ec3998770bf9894102002ffa7aba627c05338dd3
dolya-julia <juliadolya06@gmail.com>
2024-02-14 18:39:30 +0300

    lab1.task19,function5.create_unique_list_repeat_list

--- lab1/task15.py
+++ lab1/task15.py
@@ -15,8 +15,16 @@ def move_array(l):
     return l[1:]+l[:1]
 def even_odd_index(l):
     return l[::2] + l[1::2]
+def create_list(l):
+    unique_list = []
+    n_repeat_list = []
+    for elem in l:
+        if elem not in unique_list:
+            unique_list.append(elem)
+            n_repeat_list.append(l.count(elem))
+    return [unique_list, n_repeat_list]
 n = int(input())
 l = [int(input()) for i in range(n)]
 # ind = int(input())
-print(even_odd_index(l))
+print(create_list(l))
 
