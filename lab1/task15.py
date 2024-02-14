4715dacda2d93fe23bcc1dfc79236f1bd05e051f
dolya-julia <juliadolya06@gmail.com>
2024-02-14 18:22:54 +0300

    lab1.task18.function4.even_odd_index

--- lab1/task15.py
+++ lab1/task15.py
@@ -1,21 +1,22 @@
-def global_max(l, d):
-    if l[d] == max(l):
+def global_max(l, ind):
+    if l[ind] == max(l):
         return "yes"
     else:
         return "no"
-def local_min(l, d):
-    if d == 0 or d == len(l) - 1:
+def local_min(l, ind):
+    if ind == 0 or ind == len(l) - 1:
         return "no"
     else:
-        if l[d] < l[d-1] and l[d] < l[d+1]:
+        if l[ind] < l[ind-1] and l[ind] < l[ind+1]:
             return "yes"
         else:
             return "no"
 def move_array(l):
     return l[1:]+l[:1]
-
+def even_odd_index(l):
+    return l[::2] + l[1::2]
 n = int(input())
 l = [int(input()) for i in range(n)]
-# d = int(input())
-print(move_array(l))
+# ind = int(input())
+print(even_odd_index(l))
 
