05895c1ba47d71b4c9cc7035d8868e9dc24572e6
dolya-julia <juliadolya06@gmail.com>
2024-02-14 18:44:44 +0300

    lab1.task15-19.final_task

--- lab1/task15.py
+++ lab1/task15.py
@@ -25,6 +25,22 @@ def create_list(l):
     return [unique_list, n_repeat_list]
 n = int(input())
 l = [int(input()) for i in range(n)]
-# ind = int(input())
-print(create_list(l))
-
+print("Какую задачу вы хотите решить?")
+print("Задача 3, задача 15, задача 27, задача 39, задача 51")
+nm = int(input())
+if nm == 3:
+    print("Введите индекс")
+    ind = int(input())
+    print(global_max(l, ind))
+elif nm == 15:
+    print("Введите индекс")
+    ind = int(input())
+    print(local_min(l, ind))
+elif nm == 27:
+    print(move_array(l))
+elif nm == 39:
+    print(even_odd_index(l))
+elif nm == 51:
+    print(create_list(l))
+else:
+    print("Задача не выбрана")
