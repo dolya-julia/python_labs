06f1ff1fcbd314ce4e3c17e5d4f3b4d871b5648e
dolya-julia <juliadolya06@gmail.com>
2024-03-26 18:58:04 +0300

    lab7.task3.admin_panel

--- lab7/transport/app/admin.py
+++ lab7/transport/app/admin.py
@@ -2,28 +2,26 @@ from django.contrib import admin
 from .models import BusStation
 from .models import Route
 from .models import Passenger
-# from .models import PassRoutes
 
 
 class PostBusStation(admin.ModelAdmin):
     list_display = ['station_name']
+    search_fields = ['station_name']
 
 
 class PostRoute(admin.ModelAdmin):
     list_display = ['departure', 'arrival', 'departure_time', 'arrival_time', 'price']
-    list_filter = ('price', 'arrival_time', 'departure','arrival')
+    list_filter = ('price', 'departure_time', 'departure', 'arrival')
     search_fields = ['departure__station_name', 'arrival__station_name']
+    filter_horizontal = ('passengers',)
 
 
 class PostPassenger(admin.ModelAdmin):
     list_display = ['name', 'date_of_birth', 'passport']
-    list_filter = ('date_of_birth',)
-
-# class PostPassRoutes(admin.ModelAdmin):
-#     list_display = ['pass_id', 'routes_id']
+    search_fields = ['name', 'passport']
 
 
 admin.site.register(BusStation, PostBusStation)
 admin.site.register(Route, PostRoute)
 admin.site.register(Passenger, PostPassenger)
-# admin.site.register(PassRoutes, PostPassRoutes)
+
--- lab7/transport/app/models.py
+++ lab7/transport/app/models.py
@@ -8,6 +8,15 @@ class BusStation(models.Model):
         return str(self.station_name)
 
 
+class Passenger(models.Model):
+    name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    passport = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.name} {self.passport}'
+
+
 class Route(models.Model):
     departure = models.ForeignKey(BusStation, related_name='routes_as_departure', on_delete = models.CASCADE)
     arrival = models.ForeignKey(BusStation, related_name='routes_as_arrival', on_delete = models.CASCADE)
@@ -15,10 +24,9 @@ class Route(models.Model):
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
     price = models.FloatField()
+    passengers = models.ManyToManyField(Passenger)
+
+    def __str__(self):
+        return f'{self.departure} {self.arrival}'
 
 
-class Passenger(models.Model):
-    name = models.CharField(max_length=30)
-    date_of_birth = models.DateField()
-    passport = models.CharField(max_length=10)
-    routes = models.ManyToManyField(Route)
